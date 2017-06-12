f1b6fb52718998ddf56bad00eb6d46c9a3ef18fd
Robin <robin@dapplo.net>
2017-06-12 13:01:48 +0200

    Added an InvertBooleanConverter, updated dependencies. [release]

---
+++ src/Dapplo.CaliburnMicro/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,56 @@
+﻿//  Dapplo - building blocks for desktop applications
+//  Copyright (C) 2016-2017 Dapplo
+// 
+//  For more information see: http://dapplo.net/
+//  Dapplo repositories are hosted on GitHub: https://github.com/dapplo
+// 
+//  This file is part of Dapplo.CaliburnMicro
+// 
+//  Dapplo.CaliburnMicro is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  Dapplo.CaliburnMicro is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have a copy of the GNU Lesser General Public License
+//  along with Dapplo.CaliburnMicro. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Dapplo.CaliburnMicro.Converters
+{
+    /// <summary>
+    /// This helps to flip the boolean binding
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InvertBooleanConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+            bool booleanValue = (bool)value;
+            return !booleanValue;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+            bool booleanValue = (bool)value;
+            return !booleanValue;
+        }
+    }
+}
