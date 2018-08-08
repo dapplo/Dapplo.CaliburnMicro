490aaca6b4752fbd8ee9e63b6fa1a33827a4f920
Robin <robin@dapplo.net>
2018-08-08 14:28:59 +0200

    Solving build issues which occurred due to design-time classes not being available. [release]

--- src/Dapplo.CaliburnMicro.Security/DesignTime/BoolAuthentication.cs
+++ src/Dapplo.CaliburnMicro.Security/DesignTime/BoolAuthentication.cs
@@ -1,14 +1,38 @@
-﻿using System.Collections.Generic;
+﻿//  Dapplo - building blocks for desktop applications
+//  Copyright (C) 2016-2018 Dapplo
+// 
+//  For more information see: http://dapplo.net/
+//  Dapplo repositories are hosted on GitHub: https://github.com/dapplo
+// 
+//  This file is part of Dapplo.CaliburnMicro
+// 
+//  Dapplo.CaliburnMicro is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  Dapplo.CaliburnMicro is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have a copy of the GNU Lesser General Public License
+//  along with Dapplo.CaliburnMicro. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+
+using System.Collections.Generic;
 using Dapplo.CaliburnMicro.Security.Behaviors;
 
 namespace Dapplo.CaliburnMicro.Security.DesignTime
 {
-#if DEBUG
     /// <summary>
     /// A designtime implementation of INeedAuthentication for the bool (IsEnabled)
     /// </summary>
     public class BoolAuthentication : INeedAuthentication<bool>
     {
+        private BoolAuthentication()
+        {
+
+        }
         /// <inheritdoc />
         public AuthenticationTargetProperties AuthenticationTargetProperty { get; set; }
         /// <inheritdoc />
@@ -20,5 +44,4 @@ namespace Dapplo.CaliburnMicro.Security.DesignTime
         /// <inheritdoc />
         public bool WhenPermissionMissing { get; set; }
     }
-#endif
 }
--- src/Dapplo.CaliburnMicro.Security/DesignTime/VisibilityAuthentication.cs
+++ src/Dapplo.CaliburnMicro.Security/DesignTime/VisibilityAuthentication.cs
@@ -1,15 +1,39 @@
-﻿using System.Collections.Generic;
+﻿//  Dapplo - building blocks for desktop applications
+//  Copyright (C) 2016-2018 Dapplo
+// 
+//  For more information see: http://dapplo.net/
+//  Dapplo repositories are hosted on GitHub: https://github.com/dapplo
+// 
+//  This file is part of Dapplo.CaliburnMicro
+// 
+//  Dapplo.CaliburnMicro is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  Dapplo.CaliburnMicro is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have a copy of the GNU Lesser General Public License
+//  along with Dapplo.CaliburnMicro. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+
+using System.Collections.Generic;
 using System.Windows;
 using Dapplo.CaliburnMicro.Security.Behaviors;
 
 namespace Dapplo.CaliburnMicro.Security.DesignTime
 {
-#if DEBUG
     /// <summary>
     /// A designtime implementation of INeedAuthentication for the visibility
     /// </summary>
     public class VisibilityAuthentication : INeedAuthentication<Visibility>
     {
+        private VisibilityAuthentication()
+        {
+
+        }
         /// <inheritdoc />
         public AuthenticationTargetProperties AuthenticationTargetProperty { get; set; }
         /// <inheritdoc />
@@ -21,5 +45,4 @@ namespace Dapplo.CaliburnMicro.Security.DesignTime
         /// <inheritdoc />
         public Visibility WhenPermissionMissing { get; set; }
     }
-#endif
 }
