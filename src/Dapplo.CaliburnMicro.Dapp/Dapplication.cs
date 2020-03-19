66708d1dd8205e8ebb437893aac558079dd940e2
Robin Krom <robin.krom@gmail.com>
2020-03-19 22:28:30 +0100

    Fixed the package icon [skip ci]

--- src/Dapplo.CaliburnMicro.Dapp/Dapplication.cs
+++ src/Dapplo.CaliburnMicro.Dapp/Dapplication.cs
@@ -1,21 +1,21 @@
 ﻿//  Dapplo - building blocks for desktop applications
 //  Copyright (C) 2016-2019 Dapplo
-// 
+//
 //  For more information see: http://dapplo.net/
 //  Dapplo repositories are hosted on GitHub: https://github.com/dapplo
-// 
+//
 //  This file is part of Dapplo.CaliburnMicro
-// 
+//
 //  Dapplo.CaliburnMicro is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  Dapplo.CaliburnMicro is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have a copy of the GNU Lesser General Public License
 //  along with Dapplo.CaliburnMicro. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
 
@@ -100,7 +100,7 @@ namespace Dapplo.CaliburnMicro.Dapp
         protected override async void OnStartup(StartupEventArgs e)
         {
             Log.Debug().WriteLine("Starting application startup");
-            
+
             _bootstrapper.Configure();
 
             // Register the UI SynchronizationContext, this can be retrieved by specifying the name "ui" for the argument
